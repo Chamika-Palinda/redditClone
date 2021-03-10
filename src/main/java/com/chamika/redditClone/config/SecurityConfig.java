b90a78375a93eee853e3305082abc946ff1c8f2b
OfficialChamika <chamikarathnayake890@gmail.com>
2021-03-10 15:11:15 +0530

    Security Configurations

---
+++ src/main/java/com/chamika/redditClone/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.chamika.redditClone.config;
+
+//This Class For The Security Configurations
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+}
