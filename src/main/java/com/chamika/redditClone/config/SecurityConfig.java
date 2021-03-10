1210d758a1207286d48527387baf1a20d26e2291
OfficialChamika <chamikarathnayake890@gmail.com>
2021-03-10 17:59:36 +0530

    adding password encorder

--- src/main/java/com/chamika/redditClone/config/SecurityConfig.java
+++ src/main/java/com/chamika/redditClone/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.chamika.redditClone.config;
 
 //This Class For The Security Configurations
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,5 +19,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .anyRequest()
                 .authenticated();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
